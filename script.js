1a67eed5a8c6a5dbf6f0ca452421be205b1fcbe7
silvaerick13-a11y <silva.erick13@escola.pr.gov.br>
2026-04-06 11:08:05 -0300

    save

--- script.js
+++ script.js
@@ -9,4 +9,18 @@ document.addEventListener("DOMContentLoad", function()){
         secaoContato.classicList.add(contato-box);
     }
 
-});
+    //seleciona links do menu
+    const linksMenu = document.querySelector("nav a");
+    const secoes = document.querySelectorAll("section[id]");
+
+    //Deixa a transição suave nos cards via js
+    const cards = document.querySelectionAll("card");
+    for(let i= 0; i < cards.length; i++{
+        cards[i].style.transition = "0.3s ease", function(){
+            this.style.tarnsform = "translateY(-6px)";
+        });
+
+         cards[i].style.transition = "0.3s ease", function(){
+            this.style.tarnsform = "translateY(-6px)";
+            
+         });   
