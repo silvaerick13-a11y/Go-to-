1eff74e4947d401984fc9fbd208c177e115f248d
silvaerick13-a11y <silva.erick13@escola.pr.gov.br>
2026-04-06 08:28:35 -0300

    SAVE

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoad", function()){
+    //Corrige o ul da engenda caso a classe nao esteja certa
+    const secaoAgenda = document.querySelector("#agenda");
+
+
+});
